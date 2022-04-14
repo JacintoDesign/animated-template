54df3673b116440d291858968cb939a3d6f97e55
Lidia <ldelgarias@gmail.com>
2022-04-14 12:25:42 -0400

    Added ldelgarias version of the animated template project

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// AOS.int();
+AOS.init({
+    delay: 200, // values from 0 to 3000, with step 50ms
+    duration: 1500, // values from 0 to 3000, with step 50ms
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+  });
